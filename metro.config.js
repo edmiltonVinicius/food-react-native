645b81a705fb45c669ac979b2af6f90a15a06de8
Edmilton Pansanato <edmilton.pansanato@goflux.com.br>
2024-08-31 19:25:51 -0300

    running prettier

---
+++ metro.config.js
@@ -0,0 +1,9 @@
+/** @format */
+
+const path = require('path');
+const { getDefaultConfig } = require('expo/metro-config');
+const { withNativeWind } = require('nativewind/metro');
+
+const config = getDefaultConfig(path.dirname);
+
+module.exports = withNativeWind(config, { input: './src/styles/global.css' });
---
+++ src/app/_layout.tsx
@@ -0,0 +1,9 @@
+/** @format */
+
+import { Slot } from 'expo-router';
+
+import '../styles/global.css';
+
+export default function RootLayout() {
+  return <Slot />;
+}
